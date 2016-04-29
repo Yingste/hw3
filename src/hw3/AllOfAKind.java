99cccb5f4825acc63260b6aed524f74125c5cdae
Eric <yingsteric@gmail.com>
2016-04-29 13:07:16 -0500

    init

---
+++ src/hw3/AllOfAKind.java
@@ -0,0 +1,24 @@
+package hw3;
+
+/**
+ * Scoring category for a "yahtzee".  A hand
+ * with N dice satisfies this category only if all N
+ * values are the same.  For a hand that satisfies
+ * this category, the score is a fixed value specified in the constructor;
+ * otherwise, the score is zero.
+ */
+public class AllOfAKind
+{
+  /**
+   * Constructs an AllOfAKind with the given display name
+   * and score.
+   * @param displayName
+   *   name of this category
+   * @param points
+   *   points awarded for a hand that satisfies this category
+   */  
+  public AllOfAKind(String displayName, int points)
+  {
+    // TODO
+  }
+}
